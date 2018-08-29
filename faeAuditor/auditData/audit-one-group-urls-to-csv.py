ea5f13686957203e17e835002b79a5df3cefa8af
Jon Gunderson <jongund@illinois.edu>
2018-08-29 12:11:52 -0500

    added basketball home pages

--- faeAuditor/auditData/audit-one-group-urls-to-csv.py
+++ faeAuditor/auditData/audit-one-group-urls-to-csv.py
@@ -10,8 +10,9 @@ import time
 import getopt
 
 import json
-import textile
-import urllib2
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+import ssl
 
 from string import capwords
 
@@ -21,33 +22,36 @@ sys.path.append(os.path.abspath('..'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fae_audit.settings')
 
 class Domain:
-  
+
   def __init__(self, g1, u):
-  
+
     self.group1 = g1
     self.url    = u.strip()
 
 def clean(s):
-  return "".join([i for i in s if 31 < ord(i) < 127]) 
-  
+  s = s.replace('\\t', '')
+  s = s.replace('\\r', '')
+  s = s.replace('\\n', '')
+  return "".join([i for i in s if 31 < ord(i) < 127])
+
 def getDomain(s):
 
-  url_parts    = s.split("//")  
+  url_parts    = s.split("//")
   domain =  url_parts[0] + "//"
   url_parts    = url_parts[1].split("/")
   domain_parts = url_parts[0].split(".")
-  
-  
+
+
   for p in domain_parts:
     if p != 'www':
       domain += p + "."
-      
+
   domain = domain.rstrip(".")
-  
+
   return domain
 
 if len(sys.argv) < 3:
-  print "python audut-one-group-to-csv.py file1.txt file2.csv"
+  print("python audut-one-group-to-csv.py file1.txt file2.csv")
   exit()
 
 # Read in the list of the urls
@@ -58,57 +62,79 @@ file_csv  = open(sys.argv[2], 'w')
 domains    = []
 error_urls = []
 
+total = 0
 for line in file_urls:
+  total += 1
   parts = line.split(' ')
-  print(str(len(parts)))
+#  print('Line ' + str(total) + ': ' + line)
   if len(parts) == 2:
-  
+
     d =  Domain(parts[0],parts[1])
-    
+
     dup = False
     # check if domain is unique
     for item in domains:
       if item.url == d.url:
         dup = True
         break
-  
+
     if not dup:
       domains.append(d)
-    
+
 for index, item in enumerate(domains):
- 
-  try: 
-    response = urllib2.urlopen(item.url)
-    html = response.read()
-    html1 = html.lower()
-    
-    start = html1.find("<title")
-    end   = html1.find("</title")
-    body  = html1.find("<body")
-    
-    if start != -1 or body != -1:
-      title = ""
-    
-      if start != -1 and end != -1: 
-        title = html[(start+7):end]
-        title = clean(title.strip())
-      
-      line = '"NO TITLE ' + str(index) + '","' + item.url  + '","' + item.group1 + '"'
-      if title != "":  
-        line = '"' + title + '","' + item.url + '","' + item.group1 + '"'
-      
-      print str(index) + ": " + line
-    
-      file_csv.write(line + "\n")
-    else:    
-      print "*** Error no title or body tag: " + str(index) + ": " + item  
-  except:
-    print "*** Error retriving: " + str(index) + ": " + item.url  
+
+  ctx                = ssl.create_default_context()
+  ctx.check_hostname = False
+  ctx.verify_mode    = ssl.CERT_NONE
+
+  hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
+
+  title = ""
+
+  try:
+    request  = Request(item.url, headers=hdr)
+    response = urlopen(request, context=ctx)
+
+    try:
+      html = str(response.read())
+
+      try:
+        html1 = html.lower()
+        start = html1.find("<title")
+        end   = html1.find("</title")
+        body  = html1.find("<body")
+
+        if start != -1 and end != -1:
+          title = html[(start+7):end]
+          title = clean(title.strip()).strip()
+
+      except:
+        print("Parsing Error: " + str(index) + ": " + item.url)
+        error_urls.append(item)
+    except:
+      print("Read Error: " + str(index) + ": " + item.url)
+      error_urls.append(item)
+  except HTTPError as e:
+    print("HTTP Error: " + str(e.code) + " for " + str(index) + ": " + item.url)
+    item.error = str(e.code)
+    error_urls.append(item)
+  except URLError as e:
+    print("URL Error:  " + str(e.reason) + " for " + str(index) + ": " + item.url)
+    item.error = str(e.reason)
     error_urls.append(item)
 
-for index, item in enumerate(error_urls):    
-  line = '"ERROR ' + str(index) + '",' + item.url 
-  file_csv.write(line + "\n")  
+  line = '"NO TITLE ' + str(index) + '","' + item.url  + '","' + item.group1 + '"'
+  if title != "":
+    line = '"' + title + '","' + item.url + '","' + item.group1 + '"'
+
+  print(str(index) + ' of ' + str(total) + ': ' + line)
+
+  file_csv.write(line + "\n")
+
+
+for index, item in enumerate(error_urls):
+  line = '"ERROR ' + str(index) + '",' + item.url + ' (' + item.error + ')'
+  file_csv.write(line + "\n")
 
 file_csv.close()
-      
+
